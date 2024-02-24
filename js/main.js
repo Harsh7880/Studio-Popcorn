4312d630817412df71918168905c5ba18db99a0a
Harsh Kumar <harsh.kumar@robomq.io>
2024-02-24 10:19:50 +0530

    Intial Structure

---
+++ js/main.js
@@ -0,0 +1,90 @@
+
+// People Filter  show
+$(document).ready(function(){
+    
+    // nav tooggle
+    $(".hamburger-btn").click(function(){
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width() < 782){
+            $(".header .nav").slideToggle();
+        }
+    })
+    // fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".header").addClass("fixed");
+        }
+        else{
+            $(".header").removeClass("fixed");
+        }
+    })
+    // Scroll it
+
+        $.scrollIt({
+            topOffset: -50
+        });
+
+
+    //gallery popup
+    const wHeight = $(window).height();
+    $(".gallery-popup .gp-img").css("max-height",wHeight + "px");
+    
+
+    let itemIndex = 0;
+    const totalGalleryItems = $(".gallery-item").length;
+
+    $(".gallery-item").click(function(){
+        itemIndex = $(this).index();
+        $(".gallery-popup").addClass("open");
+        $(".gallery-popup .gp-img").hide();
+        gpSlideShow();
+
+    })
+ 
+    //  next And prev controls
+    $(".gp-controls .next").click(function(){
+        if(itemIndex == totalGalleryItems-1){
+            itemIndex = 0;
+        }
+        else{
+            itemIndex++;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function(){
+            gpSlideShow();
+            
+        })
+        
+    })
+    $(".gp-controls .prev").click(function(){
+        if(itemIndex === 0){
+            itemIndex = totalGalleryItems-1;
+        }
+        else{
+            itemIndex--;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function(){
+            gpSlideShow();
+            
+        })
+    })
+
+    function gpSlideShow(){
+      const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+      $(".gallery-popup .gp-img").fadeIn().attr("src",imgSrc);
+      $(".gp-counter").text((itemIndex+1) +"/"+ totalGalleryItems );
+    }
+
+    // hide gallery Popup
+    $(".gp-close").click(function(){
+        $(".gallery-popup").removeClass("open");
+    })
+
+    // hide gallery popup  when clickes outside of gp-container
+    $(".gallery-popup").click(function(event){
+        if($(event.target).hasClass("open")){
+            $(".gallery-popup").removeClass("open");
+        }
+    })
+})
