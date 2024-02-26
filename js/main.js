52f07e85243ff9d406a92c46495499072e5b661a
Harsh Kumar <harsh.kumar@robomq.io>
2024-02-26 21:46:14 +0530

    about-us pop-up added

--- js/main.js
+++ js/main.js
@@ -19,93 +19,102 @@ function closeForm() {
       }
     });
     });
+
+    
   
 // People Filter  show
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     // nav tooggle
-    $(".hamburger-btn").click(function(){
+    $(".hamburger-btn").click(function () {
         $(".header .nav").slideToggle();
     })
-    $(".header .nav a").click(function(){
-        if($(window).width() < 782){
+    $(".header .nav a").click(function () {
+        if ($(window).width() < 782) {
             $(".header .nav").slideToggle();
         }
     })
     // fixed header
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 100){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
             $(".header").addClass("fixed");
         }
-        else{
+        else {
             $(".header").removeClass("fixed");
         }
     })
-    // Scroll it
+    // // Scroll it
 
-        $.scrollIt({
-            topOffset: -50
-        });
+    // $.scrollIt({
+    //     topOffset: -20
+    // });
 
 
     //gallery popup
     const wHeight = $(window).height();
-    $(".gallery-popup .gp-img").css("max-height",wHeight + "px");
-    
+    $(".gallery-popup .gp-img").css("max-height", wHeight + "px");
+
 
     let itemIndex = 0;
     const totalGalleryItems = $(".gallery-item").length;
 
-    $(".gallery-item").click(function(){
+    $(".gallery-item").click(function () {
         itemIndex = $(this).index();
         $(".gallery-popup").addClass("open");
         $(".gallery-popup .gp-img").hide();
         gpSlideShow();
 
     })
- 
+
     //  next And prev controls
-    $(".gp-controls .next").click(function(){
-        if(itemIndex == totalGalleryItems-1){
+    $(".gp-controls .next").click(function () {
+        if (itemIndex == totalGalleryItems - 1) {
             itemIndex = 0;
         }
-        else{
+        else {
             itemIndex++;
         }
-        $(".gallery-popup .gp-img").fadeOut(function(){
+        $(".gallery-popup .gp-img").fadeOut(function () {
             gpSlideShow();
-            
+
         })
-        
+
     })
-    $(".gp-controls .prev").click(function(){
-        if(itemIndex === 0){
-            itemIndex = totalGalleryItems-1;
+    $(".gp-controls .prev").click(function () {
+        if (itemIndex === 0) {
+            itemIndex = totalGalleryItems - 1;
         }
-        else{
+        else {
             itemIndex--;
         }
-        $(".gallery-popup .gp-img").fadeOut(function(){
+        $(".gallery-popup .gp-img").fadeOut(function () {
             gpSlideShow();
-            
+
         })
     })
 
-    function gpSlideShow(){
-      const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
-      $(".gallery-popup .gp-img").fadeIn().attr("src",imgSrc);
-      $(".gp-counter").text((itemIndex+1) +"/"+ totalGalleryItems );
+    function gpSlideShow() {
+        const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+        $(".gallery-popup .gp-img").fadeIn().attr("src", imgSrc);
+        $(".gp-counter").text((itemIndex + 1) + "/" + totalGalleryItems);
     }
 
     // hide gallery Popup
-    $(".gp-close").click(function(){
+    $(".gp-close").click(function () {
         $(".gallery-popup").removeClass("open");
     })
 
     // hide gallery popup  when clickes outside of gp-container
-    $(".gallery-popup").click(function(event){
-        if($(event.target).hasClass("open")){
+    $(".gallery-popup").click(function (event) {
+        if ($(event.target).hasClass("open")) {
             $(".gallery-popup").removeClass("open");
         }
     })
+
+    $(".Click-here").on('click', function () {
+        $(".custom-model-main").addClass('model-open');
+    });
+    $(".close-btn, .bg-overlay").click(function () {
+        $(".custom-model-main").removeClass('model-open');
+    });
 })
