85c862a923d0a38705b3445f2c1269844487fd78
Harsh Kumar <harsh.kumar@robomq.io>
2024-02-26 16:57:24 +0530

    Contact form Added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-
+function closeForm() {
+    $('.form-popup-bg').removeClass('is-visible');
+  }
+  
+  $(document).ready(function($) {
+    
+    /* Contact Form Interactions */
+    $('#btnOpenForm').on('click', function(event) {
+      event.preventDefault();
+  
+      $('.form-popup-bg').addClass('is-visible');
+    });
+    
+      //close popup when clicking x or off popup
+    $('.form-popup-bg').on('click', function(event) {
+      if ($(event.target).is('.form-popup-bg') || $(event.target).is('#btnCloseForm')) {
+        event.preventDefault();
+        $(this).removeClass('is-visible');
+      }
+    });
+    });
+  
 // People Filter  show
 $(document).ready(function(){
     
