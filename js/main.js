e2742a3ae00d5423e997dd94103f464ab1013cd5
Harsh Kumar <vijayk8478@gmail.com>
2024-05-09 13:02:59 +0530

    Refactored JavaScript

--- js/main.js
+++ js/main.js
@@ -1,81 +1,66 @@
-// function closeForm() {
-//   $('.form-popup-bg').removeClass('is-visible');
-// }
+// Book Now Form
 
-// $(document).ready(function ($) {
+// Function to close the form popup
+function closeForm() {
+  document.querySelector(".form-popup-bg").classList.remove("is-visible");
+}
 
-//   /* Contact Form Interactions */
-//   $('#btnOpenForm').on('click', function (event) {
-//     event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  var btnOpenForm = document.getElementById("btnOpenForm");
+  var formPopupBg = document.querySelector(".form-popup-bg");
 
-//     $('.form-popup-bg').addClass('is-visible');
-//   });
+  // Open form when button is clicked
+  btnOpenForm.addEventListener("click", function (event) {
+    event.preventDefault();
+    formPopupBg.classList.add("is-visible");
+  });
 
-//   //close popup when clicking x or off popup
-//   $('.form-popup-bg').on('click', function (event) {
-//     if ($(event.target).is('.form-popup-bg') || $(event.target).is('#btnCloseForm')) {
-//       event.preventDefault();
-//       $(this).removeClass('is-visible');
-//     }
-//   });
-// });
-
-
-
-// People Filter  show
-// $(document).ready(function () {
-
-//   // nav tooggle
-//   $(".hamburger-btn").click(function () {
-//     $(".header .nav").slideToggle();
-//   })
-//   $(".header .nav a").click(function () {
-//     if ($(window).width() < 782) {
-//       $(".header .nav").slideToggle();
-//     }
-//   })
+  // Close form when clicking outside or on close button
+  formPopupBg.addEventListener("click", function (event) {
+    if (
+      event.target.classList.contains("form-popup-bg") ||
+      event.target.id === "btnCloseForm"
+    ) {
+      event.preventDefault();
+      closeForm();
+    }
+  });
+});
 
 // Function to toggle the navigation menu
 function toggleNav() {
-  var nav = document.querySelector('.header .nav');
-  if (nav.style.display === 'block') {
-    nav.style.display = 'none';
+  var nav = document.querySelector(".header .nav");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
   } else {
-    nav.style.display = 'block';
+    nav.style.display = "block";
   }
 }
 
 // Event listener for the hamburger button click
-document.querySelector('.hamburger-btn').addEventListener('click', toggleNav);
+document.querySelector(".hamburger-btn").addEventListener("click", toggleNav);
 
 // Event listener for navigation links click
-var navLinks = document.querySelectorAll('.header .nav a');
-navLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
+var navLinks = document.querySelectorAll(".header .nav a");
+navLinks.forEach(function (link) {
+  link.addEventListener("click", function () {
     if (window.innerWidth < 782) {
       toggleNav();
     }
   });
 });
-  // // fixed header
-  // $(window).scroll(function () {
-  //   if ($(this).scrollTop() > 100) {
-  //     $(".header").addClass("fixed");
-  //   }
-  //   else {
-  //     $(".header").removeClass("fixed");
-  //   }
-  // })
 
-  window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    if (window.scrollY > 20) {
-      header.classList.add("fixed");
-    } else {
-      header.classList.remove("fixed");
-    }
-  });
-  // // Scrol// Function to handle smooth scrolling
+// Scroll Fixed
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  if (window.scrollY > 20) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+// Scroll Function to handle smooth scrolling
 function scrollToSection(event) {
   event.preventDefault();
   // Get the target section's ID from the clicked link
@@ -85,11 +70,12 @@ function scrollToSection(event) {
   // Calculate the offset based on the topOffset value
   var offset = -50; // Set the desired topOffset value
   // Calculate the target position by adding the offset to the target section's top position
-  var targetPosition = targetSection.getBoundingClientRect().top + window.pageYOffset + offset;
+  var targetPosition =
+    targetSection.getBoundingClientRect().top + window.pageYOffset + offset;
   // Scroll to the target position smoothly
   window.scrollTo({
     top: targetPosition,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 }
 
@@ -97,159 +83,168 @@ function scrollToSection(event) {
 var navLinks = document.querySelectorAll('a[href^="#"]');
 
 // Add click event listeners to each navigation link
-navLinks.forEach(function(link) {
-  link.addEventListener('click', scrollToSection);
+navLinks.forEach(function (link) {
+  link.addEventListener("click", scrollToSection);
 });
 
-//   $.scrollIt({
-//     topOffset: -50
-//   });
+// Know More custom model on About us section
+document.addEventListener("DOMContentLoaded", function () {
+  var clickHereButton = document.querySelector(".Click-here");
+  var closeButton = document.querySelector(".close-btn");
+  var bgOverlay = document.querySelector(".bg-overlay");
+  var customModelMain = document.querySelector(".custom-model-main");
 
+  // Open modal when "Click here" button is clicked
+  clickHereButton.addEventListener("click", function () {
+    customModelMain.classList.add("model-open");
+  });
 
-//   //gallery popup
-//   const wHeight = $(window).height();
-//   $(".gallery-popup .gp-img").css("max-height", wHeight + "px");
-
-
-//   let itemIndex = 0;
-//   const totalGalleryItems = $(".gallery-item").length;
-
-//   $(".gallery-item").click(function () {
-//     itemIndex = $(this).index();
-//     $(".gallery-popup").addClass("open");
-//     $(".gallery-popup .gp-img").hide();
-//     gpSlideShow();
-
-//   })
-
-//   //  next And prev controls
-//   $(".gp-controls .next").click(function () {
-//     if (itemIndex == totalGalleryItems - 1) {
-//       itemIndex = 0;
-//     }
-//     else {
-//       itemIndex++;
-//     }
-//     $(".gallery-popup .gp-img").fadeOut(function () {
-//       gpSlideShow();
-
-//     })
-
-//   })
-//   $(".gp-controls .prev").click(function () {
-//     if (itemIndex === 0) {
-//       itemIndex = totalGalleryItems - 1;
-//     }
-//     else {
-//       itemIndex--;
-//     }
-//     $(".gallery-popup .gp-img").fadeOut(function () {
-//       gpSlideShow();
-
-//     })
-//   })
-
-//   function gpSlideShow() {
-//     const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
-//     $(".gallery-popup .gp-img").fadeIn().attr("src", imgSrc);
-//     $(".gp-counter").text((itemIndex + 1) + "/" + totalGalleryItems);
-//   }
-
-//   // hide gallery Popup
-//   $(".gp-close").click(function () {
-//     $(".gallery-popup").removeClass("open");
-//   })
-
-//   // hide gallery popup  when clickes outside of gp-container
-//   $(".gallery-popup").click(function (event) {
-//     if ($(event.target).hasClass("open")) {
-//       $(".gallery-popup").removeClass("open");
-//     }
-//   })
-
-//   $(".Click-here").on('click', function () {
-//     $(".custom-model-main").addClass('model-open');
-//   });
-//   $(".close-btn, .bg-overlay").click(function () {
-//     $(".custom-model-main").removeClass('model-open');
-//   });
-// })
-
-// });
-
-new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  cssMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  loop: true,
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    300: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    }
-  },
-});
-new Swiper(".mySwiper1", {
-  slidesPerView: 4,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  cssMode: true,
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    300: {
-      slidesPerView: 1,
-      spaceBetween: 50,
-      loop: true,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1400: {
-      slidesPerView: 4,
-    }
-  },
-});
-new Swiper(".mySwiper3", {
-  slidesPerView: 2,
-  spaceBetween: 20,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  cssMode: true,
-  loop: true,
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    300: {
-      slidesPerView: 1,
-    },
-    640: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 2,
+  // Close modal when close button or background overlay is clicked
+  function closeModel(event) {
+    if (event.target === closeButton || event.target === bgOverlay) {
+      customModelMain.classList.remove("model-open");
     }
   }
+
+  closeButton.addEventListener("click", closeModel);
+  bgOverlay.addEventListener("click", closeModel);
+});
+
+// Swiper Slider's for triners, wedding-gallery, and testimonial's
+// Function to initialize Swiper slider
+function initializeSwiper(selector, options) {
+  var swiper = new Swiper(selector, options);
 }
-)
+
+// Initialize Swiper sliders
+document.addEventListener("DOMContentLoaded", function () {
+  initializeSwiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    cssMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    loop: true,
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+      300: { slidesPerView: 1 },
+      640: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+    },
+  });
+
+  initializeSwiper(".mySwiper1", {
+    slidesPerView: 4,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    cssMode: true,
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+      300: { slidesPerView: 1, spaceBetween: 50, loop: true },
+      640: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+      1400: { slidesPerView: 4 },
+    },
+  });
+
+  initializeSwiper(".mySwiper3", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    cssMode: true,
+    loop: true,
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+      300: { slidesPerView: 1 },
+      640: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+    },
+  });
+});
+
+// Wedding gallery Image Pop-Up
+
+// Get the window height
+const wHeight = window.innerHeight;
+document.querySelectorAll(".gallery-popup .gp-img").forEach(function (gpImg) {
+  gpImg.style.maxHeight = wHeight + "px";
+});
+
+// Initialize itemIndex and totalGalleryItems
+let itemIndex = 0;
+const galleryItems = document.querySelectorAll(".gallery-item");
+const totalGalleryItems = galleryItems.length;
+
+// Event listener for clicking on gallery items
+galleryItems.forEach(function (galleryItem) {
+  galleryItem.addEventListener("click", function () {
+    itemIndex = Array.from(galleryItems).indexOf(this);
+    document.querySelector(".gallery-popup").classList.add("open");
+    document.querySelector(".gallery-popup .gp-img").style.display = "none";
+    gpSlideShow();
+  });
+});
+
+// Event listener for next and prev controls
+document
+  .querySelector(".gp-controls .next")
+  .addEventListener("click", function () {
+    if (itemIndex === totalGalleryItems - 1) {
+      itemIndex = 0;
+    } else {
+      itemIndex++;
+    }
+    document.querySelector(".gallery-popup .gp-img").style.display = "none";
+    gpSlideShow();
+  });
+
+document
+  .querySelector(".gp-controls .prev")
+  .addEventListener("click", function () {
+    if (itemIndex === 0) {
+      itemIndex = totalGalleryItems - 1;
+    } else {
+      itemIndex--;
+    }
+    document.querySelector(".gallery-popup .gp-img").style.display = "none";
+    gpSlideShow();
+  });
+
+// Function to handle the gallery popup slideshow
+function gpSlideShow() {
+  const imgSrc = galleryItems[itemIndex]
+    .querySelector("img")
+    .getAttribute("data-large");
+  const gpImg = document.querySelector(".gallery-popup .gp-img");
+  gpImg.src = imgSrc;
+  gpImg.style.display = "block";
+  document.querySelector(".gp-counter").textContent =
+    itemIndex + 1 + "/" + totalGalleryItems;
+}
+
+// Event listener to hide gallery popup when the close button is clicked
+document.querySelector(".gp-close").addEventListener("click", function () {
+  document.querySelector(".gallery-popup").classList.remove("open");
+});
+
+// Event listener to hide gallery popup when clicked outside of gp-container
+document
+  .querySelector(".gallery-popup")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("open")) {
+      document.querySelector(".gallery-popup").classList.remove("open");
+    }
+  });
